5fd39a065b62234a18830f4037ae16a98a27689c
Lucas Parry <lucas@envato.com>
2009-09-15 16:57:04 +1000

    done thinking about triangles

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a,b,c].sort
+  
+  sides.reject! { |item| item <= 0 }
+  raise TriangleError if sides.size < 3 #if we lost any sides because they were >= 0
+  
+  raise TriangleError if sides[2] >= sides[0] + sides[1] # if the biggest side is >= than the sum of the smaller sides
+  
+  return :equilateral if sides.uniq.size == 1
+  return :isosceles if sides.uniq.size == 2
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
